750a02147e076f266b4c20ad5d171b2b8d1f1eb9
Bouba <boubacar.konate@outlook.fr>
2023-11-22 15:12:41 +0100

    index.php décomposé

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<hr>
+    <footer>
+        <p> &copy; 2023 - GRPG FORMATION</p>
+    </footer>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,10 @@
+<header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Exemple1</a></li>
+                <li><a href="#">Exemple2</a> </li>
+                <li><a href="#">Exemple3</a></li>
+            </ul>
+        </nav>
+    </header>
