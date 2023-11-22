d9944d77c731bb0fccb52fc813a3decd113d1c2c
Bouba <boubacar.konate@outlook.fr>
2023-11-22 15:09:06 +0100

    index.php et style css ok

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP exemples</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Exemple1</a></li>
+                <li><a href="#">Exemple2</a> </li>
+                <li><a href="#">Exemple3</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Exemples avec PHP</h1>
+    </main>
+    <hr>
+    <footer>
+        <p> &copy; 2023 - GRPG FORMATION</p>
+    </footer>
+</body>
+</html>
